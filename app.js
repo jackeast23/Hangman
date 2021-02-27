e61244a2f2099c1069c08b78f2036b8aeeeaaf9a
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-02-26 19:57:32 -0500

    added HTTP requests

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// HTTP (Hypertext Transfer Protocol)
+// Request - What do we want to do
+// Response - What was actually done
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const statusEl = document.querySelector('#status')
@@ -15,3 +19,16 @@ window.addEventListener('keypress', function (e) {
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
